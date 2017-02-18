04fe07b3baabb4403aa3d4d1570da24467514480
hahnmiao <hahnmiao@gmail.com>
2017-02-17 22:30:47 -0800

    Update install/vim.sh

--- install/vim.sh
+++ install/vim.sh
@@ -1,24 +1,22 @@
 # Set brew macvim to be the default
 brew link --overwrite macvim
 
-echo "creating vim directories"
+brew tap neovim/homebrew-neovim
+brew install --HEAD neovim
+
+ln -s /Users/hahn/Development/Github/dotfiles/.vimrc  ~/.config/nvim/init.vim
+
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# install vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 mkdir -p ~/.vim/colors
 mkdir ~/.vim/plugin
 
-# setp up Vundle
-mkdir ~/.vim/bundle/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Run :NeoBundleInstall in vim"
+
 ln -s $HOME/Dropbox/Github/dotfiles/.vim/snippets $HOME/.vim/snippets
-
-# pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-# compile ycm
-brew install CMake
-cd ~/.vim/bundle/YouCompleteMe
-./install.sh --clang-completer --system-libclang
 
 # ctrlp.vim:
 # 1. Clone the plugin into a separate directory:
@@ -26,10 +24,8 @@ cd ~/.vim
 # 2.
 git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
 
-
-
-# Switch to NeoVim
-mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -s ~/.vim $XDG_CONFIG_HOME/nvim
-ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+# # Switch to NeoVim
+# mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+# ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+# ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 
