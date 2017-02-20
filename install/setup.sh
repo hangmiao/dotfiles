a410350ad28f6752d09e2cac198b863bb3fe7d47
hahnmiao <hahnmiao@gmail.com>
2017-02-19 17:21:38 -0800

    Update things

--- install/setup.sh
+++ install/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # curl -L http://install.ohmyz.sh | sh
+# cp $HOME/Dropbox/Github/dotfiles/.oh-my-zsh/themes/robbyrussell.zsh-theme $HOME/.oh-my-zsh/themes/robbyrussell.zsh-theme
 
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+cp ~/Dropbox/Github/dotfiles/.zprezto/modules/prompt/functions/prompt_hahn_setup ~/.zprezto/modules/prompt/functions/prompt_hahn_setup
 
 mkdir ~/bin
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl ~/bin/subl
